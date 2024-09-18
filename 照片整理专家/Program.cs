751eb1eca71c214000215cf4124fed5eaae0bf5d
nuoyan_cfan <nuoyan_cfan@163.com>
2024-09-18 09:39:16 +0800

    取消以管理员身份运行功能，解决以管理员身份运行时不支持拖拽的问题。

--- 照片整理专家/Program.cs
+++ 照片整理专家/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Reflection;
-using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,33 +14,9 @@ namespace 照片整理专家
         [STAThread]
         static void Main()
         {
-            Action run = () =>
-            {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new frmFramework());
-            };
-            WindowsIdentity wi = WindowsIdentity.GetCurrent();
-            bool runAsAdmin = wi != null && new WindowsPrincipal(wi).IsInRole(WindowsBuiltInRole.Administrator);
-            if (!runAsAdmin)
-            {
-                try
-                {
-                    //不可能以管理员方式直接启动一个 ClickOnce 部署的应用程序，所以尝试以管理员方式启动一个新的进程
-                    Process.Start(new ProcessStartInfo(Assembly.GetExecutingAssembly().CodeBase) { UseShellExecute = true, Verb = "runas" });
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(string.Format("以管理员方式启动失败，将尝试以普通方式启动。{0}{1}", Environment.NewLine, ex), "以管理员方式启动失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    run();//以管理员方式启动失败，则尝试普通方式启动
-                }
-                Application.Exit();
-            }
-            else
-            {
-                run();
-            }
-
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmFramework());
         }
     }
 }
